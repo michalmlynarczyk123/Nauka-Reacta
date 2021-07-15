1fc9649a198285ec1d0679e80510a77e2a6536fd
Michał Młynarczyk <michalmlyn@gmail.com>
2021-07-15 13:41:05 +0200

    columny

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero.js';
+import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+    image: PropTypes.string,
+  }
+
+  static defaultProps = {
+    children: <p>Interesting things I want to check out!</p>
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero/>
+        <Hero titleText={this.props.title} 
+              imageImg={this.props.image} />
+        <div className={styles.description}>
+            {this.props.children}
+        </div>
+
+        <div className={styles.columns}>
+          <Column columnTitle={'Animals'}></Column>
+          <Column columnTitle={'Plants'}></Column>
+          <Column columnTitle={'Minerals'}></Column>
+        </div>
       </section>
-    )
-  }
+        )
+    }
 }
 
 export default List;
